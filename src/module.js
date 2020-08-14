9ad4e1af1f48156dac027d04d265261fb14f06ba
Jiří Kofránek <kofranek@gmail.com>
2020-08-15 00:19:37 +0200

    Modul instalace webpacku - přidání ESlint

--- src/module.js
+++ src/module.js
@@ -1,6 +1,7 @@
 console.log('module.js')
 
-async function start(){
+async function start() {
     return await Promise.resolve('async is working')
 }
+
 start().then(console.log)
--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,51 @@
-//nastavení cesty:
+// nastavení cesty:
 const path=require('path')
 const {CleanWebpackPlugin} = require('clean-webpack-plugin')
-const HTMLWebpackPlugin = require ('html-webpack-plugin')
-const CopyWebpackPlugin = require ('copy-webpack-plugin')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 const isProd=process.env.NODE_env === 'production'
 const isDev = !isProd
 
-console.log('isProd', isProd)
-console.log('isDev', isDev)
-
 const filename = ext => isDev ? `bundle.${ext}`:`bundle.[hash].${ext}`
+
+const jsLoaders = () =>{
+    const loaders = [{
+            loader: 'babel-loader',
+            options: {
+                presets: ['@babel/preset-env'],
+            }
+        }]
+    if (isDev) {
+        loaders.push('eslint-loader')
+    }
+    return loaders
+}
 
 module.exports = {
     //určení kde je zdroják:
     context: path.resolve(__dirname, 'src'),
-    mode: "development",
+    mode: 'development',
     //vstupní bod:
-    entry: ['@babel/polyfill','./index.js'],
+    entry: ['@babel/polyfill', './index.js'],
     output: {
         //název výstupního souboru:
         filename: filename( 'js'),
         //vytvoření výstupní složky 'dist'
-        path: path.resolve(__dirname,'dist')
+        path: path.resolve(__dirname, 'dist')
     },
-    resolve:{
+    resolve: {
         extensions: ['.js'],
-        alias:{
-            '@':path.resolve(__dirname,'src'),
-            '@core': path.resolve(__dirname,'src/core')
+        alias: {
+            '@': path.resolve(__dirname, 'src'),
+            '@core': path.resolve(__dirname, 'src/core')
         }
     },
     devtool: isDev ? 'source-map' : false,
     devServer: {
         port: 3000,
-        hot:isDev
+        hot: isDev
     },
     plugins: [
         new CleanWebpackPlugin(),
@@ -43,14 +53,14 @@ module.exports = {
         new CopyWebpackPlugin({
             patterns: [
                 {
-                    from: path.resolve(__dirname,'src/favicon.ico'),
-                    to: path.resolve(__dirname,'dist')
+                    from: path.resolve(__dirname, 'src/favicon.ico'),
+                    to: path.resolve(__dirname, 'dist')
                 }
             ]
             }
         ),
         new MiniCssExtractPlugin({
-            filename: filename ('css')
+            filename: filename('css')
         })
     ],
     module: {
@@ -69,18 +79,12 @@ module.exports = {
                     'sass-loader'
                 ]
             },
-            { test: /\.js$/,
+            {test: /\.js$/,
                 exclude: /node_modules/,
-                loader: {
-                    loader:'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env']
-                    }
-                }
+                use: jsLoaders(),
+
             }
 
         ]
     }
-
-
 }
