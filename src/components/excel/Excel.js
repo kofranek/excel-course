c8bb2e6806be809710987fbe4d33920badccdf4a
Jiří Kofránek <kofranek@gmail.com>
2020-08-18 00:00:32 +0200

    Framework: Plánování a vytvoření základní OOP struktury

--- src/components/excel/Excel.js
+++ src/components/excel/Excel.js
@@ -1,6 +1,27 @@
 export class Excel {
     constructor(selector, options) {
-        this.$el=document.querySelectorAll(selector)
-        this.components = options.components||[]
+        this.$el = document.querySelector(selector)
+        this.components = options.components || []
+    }
+
+    getRoot() {
+        const $root = document.createElement('div')
+        this.components.forEach(Component => {
+            const component = new Component()
+            $root.insertAdjacentHTML('beforeend', component.toHTML())
+        })
+        return $root
+    }
+
+
+    render() {
+        //'afterbegin','afterend','beforebegin','beforeend'
+        //this.$el.insertAdjacentHTML('beforebegin', '<strong>test</strong>');
+
+        // const node=document.createElement('h1')
+        // node.textContent='TEXT'
+        // this.$el.append(node)
+        this.getRoot()
+        this.$el.append(this.getRoot())
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import './scss/index.scss'
 import {Excel} from '@/components/excel/Excel';
+import {Header} from '@/components/header/Header';
+import {Toolbar} from '@/components/toolbar/Toolbar';
+import {Formula} from '@/components/formula/Formula';
+import {Table} from '@/components/table/Table';
 
-const excel=new Excel('#app', {
-    components: []
+const excel = new Excel('#app', {
+     components: [Header, Toolbar, Formula, Table]
 })
 
-console.log('Excel', excel);
+excel.render()
