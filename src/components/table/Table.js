10473ac41aec8906db86704d4fb7d4c86bb175e3
Jiří Kofránek <kofranek@gmail.com>
2020-09-26 22:50:27 +0200

    Framework: Sestavení tabulky. Buňky

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -4,6 +4,7 @@ import {createTable} from '@/components/table/table.tempate';
 export class Table extends ExcelComponent {
     static className = 'excel__table'
     toHTML() {
-        return createTable()
+        return createTable(20)
     }
 }
+
--- src/components/table/table.tempate.js
+++ src/components/table/table.tempate.js
@@ -1,25 +1,25 @@
-/* eslint-disable */
 const CODES = {
     A: 65,
     Z: 90
 }
 
-function createCell(){
+function toCell() {
     return `
-      <div class="cell" contenteditable="true">A1</div>
+      <div class="cell" contenteditable="true"></div>
     `
 }
 
-function toColumn(col){
+
+function toColumn(col) {
     return `
       <div class="column">${col}</div>
     `
 }
 
-function createRow(content=''){
-    return`  
+function createRow(index='', content='') {
+    return `  
       <div class="row">
-          <div class="row-info"></div>
+          <div class="row-info">${index}</div>
           <div class="row-data">${content}</div>
         </div>
     `
@@ -29,6 +29,7 @@ export function createTable(rowCount=15) {
     const colsCount = CODES.Z-CODES.A+1
     const rows = []
 
+    // méně elegantní řešení
     // const cols = new Array(colsCount)
     //     .fill('*')
     //     .map((el,index)=>{
@@ -39,24 +40,40 @@ export function createTable(rowCount=15) {
     //     })
     //     .join('')
 
-    function toChar(_,index){
+    function toChar(_, index) {
         return String.fromCharCode(CODES.A+index)
     }
 
     const cols = new Array(colsCount)
-        .fill('*')
+        .fill('')
         .map(toChar)
         .map(toColumn)
         .join('')
 
-    rows.push(createRow(cols))
+    rows.push(createRow('', cols))
 
 
-    for (let i=0; i<rowCount; i++){
-        rows.push(createRow(''))
+    // moje řešení:
+    // const cells=new Array(colsCount)
+    //     .fill('')
+    //     // .map((el)=>{
+    //     //     return toCell()
+    //     // })
+    //     .map(toCell)
+    //     .join('')
+    //
+    // for (let i=0; i<rowCount; i++) {
+    //     rows.push(createRow(i+1, cells))
+    // }
+
+    for (let i=0; i<rowCount; i++) {
+        const cells=new Array(colsCount)
+            .fill('')
+            .map(toCell)
+            .join('')
+        rows.push(createRow(i+1, cells))
     }
 
     return rows.join('')
-
 }
 
