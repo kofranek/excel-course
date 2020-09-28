f68f5993aeb98119bf6972b6fb0be3d9d78ca781
Jiří Kofránek <kofranek@gmail.com>
2020-09-28 22:03:33 +0200

    Table resize: vytvoření prvku pro resize

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -7,29 +7,13 @@ export class Table extends ExcelComponent {
     constructor($root) {
         super($root, {
             name: 'Table',
-            listeners: ['click', 'mousedown', 'mousemove', 'mouseup']
+           // listeners: ['click', 'mousedown', 'mousemove', 'mouseup']
         })
     }
 
 
     toHTML() {
         return createTable(20)
-    }
-
-    onClick(event) {
-        console.log('click', event.target);
-    }
-
-    onMousedown(event) {
-        console.log('mousedown', event.target);
-    }
-
-    onMousemove() {
-        console.log('mousemove');
-    }
-
-    onMouseup() {
-        console.log('mouseup');
     }
 }
 
